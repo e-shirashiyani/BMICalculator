5dafa9ed59e73a9f12102aa6c0e5302e64ec706c
e.shirashiyani <e.shirashiyani>
2022-02-08 11:28:19 +0330

    adding result of calculation

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -15,6 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
 
+    
+    @IBAction func WeightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)kg"
+    }
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
 }
 
